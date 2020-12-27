782cc899af46c67004f601c51c27a4242e5d831d
Batuhan ÖZALP <33182788+bozalp@users.noreply.github.com>
2020-12-27 17:14:49 +0300

    Coin Collect Animation

---
+++ Scripts/CoinManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinManager : MonoBehaviour
+{
+    private Text coin_txt;
+    private Camera cam;
+    private Vector3 target_pos;
+    private Image target;
+    private bool isAnimation;
+    private static int total_coin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        total_coin = 0;
+        cam = Camera.main;
+        isAnimation = false;
+        target = GameObject.Find("coin_target").GetComponent<Image>();
+        coin_txt = GameObject.Find("coin_txt").GetComponent<Text>();
+        coin_txt.text = total_coin.ToString();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = Vector3.Distance(transform.position, target_pos);
+        if (distance < 1f)
+        {
+            Destroy(gameObject);
+        }
+        else if (isAnimation)
+        {
+            PlayAnimation();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            target_pos = Target();
+            isAnimation = true;
+            total_coin++;
+            coin_txt.text = total_coin.ToString();
+        }
+    }
+    private Vector3 Target()
+    {
+        Vector3 ps = target.rectTransform.position;
+        ps.z = 10;//You can make this value an average of 50 so that the money shrinks as it goes to the target.
+        return cam.ScreenToWorldPoint(ps);
+    }
+    private void PlayAnimation()
+    {
+        transform.position = Vector3.Lerp(transform.position, target_pos, Time.deltaTime * 5);
+    }
+}
---
+++ Scripts/character.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class character : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //    coin.transform.position = cam.ScreenToWorldPoint(new Vector3(coin_target.transform.position.x,
+        //           coin_target.transform.position.y, 0));
+        //    Vector3 screen_pos = cam.ScreenToWorldPoint(coin.transform.position);
+        //    Vector3 screen_pos2 = cam.ScreenToWorldPoint(coin_target.transform.position);
+        //    print(screen_pos);
+        //    print(screen_pos2);
+
+        //Vector3 screen_pos2 = cam.WorldToScreenPoint(coin_target.transform.position);
+        //print(screen_pos2);
+       
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+            rb.AddForce(0, 250, 0);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            rb.transform.position += new Vector3(5 * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rb.transform.position += new Vector3(-5 * Time.deltaTime, 0, 0);
+
+
+
+        //Vector3 screen_pos = cam.WorldToScreenPoint(coin.transform.position);
+        //Vector3 screen_pos2 = cam.WorldToScreenPoint(coin_target.transform.position);
+        //coin.transform.position = Vector3.Lerp(screen_pos, screen_pos2, .000001f);
+
+            //GO.transform.position = Vector3.Lerp(coin.transform.position, point, .01f);
+        
+    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.gameObject.tag == "coin")
+    //    {
+    //        coin_pos = other.transform.position;
+    //        print(coin_pos);
+    //            other.transform.position = Vector3.Lerp(coin_pos, target_pos, Time.deltaTime * 1);
+    //        // point = cam.ScreenToWorldPoint(new Vector3(coin_pos.position.x, target_pos.position.x, cam.transform.position.z));
+    //        //// coin_pos.position = target_pos.position;
+    //        // //float time = 0;
+    //        // //while (time < 10)
+    //        // //{
+    //        // //time += .002f * Time.deltaTime;
+    //        time = 15;
+    //       // GO = Instantiate(coin, coin.transform);
+    //        //}
+    //        //Destroy(other.gameObject);
+    //    }
+    //}
+}
